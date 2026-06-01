150eafca23cb8eb8e1fb0cca6e1afbebfb269379
jayy-codes07 <hadiyajay2010@gmail.com>
2026-06-01 16:57:02 +0530

    add header tokens in middlware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,29 +1,32 @@
-import jwt from "jsonwebtoken"
-import { ApiError } from "../utility/apiError.js"
-import { Doctor } from "../models/doctor.model.js"
-import { asyncHandler } from "../utility/asyncHandler.js"
+import jwt from "jsonwebtoken";
+import { ApiError } from "../utility/apiError.js";
+import { Doctor } from "../models/doctor.model.js";
+import { asyncHandler } from "../utility/asyncHandler.js";
 
 export const verifyJWT = asyncHandler(async (req, res, next) => {
-    const token = req?.cookies.Accesstoken
+  const token =
+    req?.cookies.Accesstoken ||
+    req.headers.authorization?.replace("Bearer ", "");
 
-    if (!token) {
-        throw new ApiError(400, "unauthorized user")
-    }
+  if (!token) {
+    throw new ApiError(400, "unauthorized user");
+  }
 
+  const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  if (!verifyToken) {
+    throw new ApiError(400, "Accesstoken is incorrect");
+  }
+  const userid = verifyToken?.id;
 
-    const verifyToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-    if (!verifyToken) {
-        throw new ApiError(400, "Accesstoken is incorrect")
-    }
-    const userid = verifyToken?.id
+  const doctor = await Doctor.findById(userid).select(
+    "-password -refreshToken",
+  );
 
-    const doctor = await Doctor.findById(userid).select('-password -refreshToken')
+  if (!doctor) {
+    throw new ApiError(400, "doctor does not found");
+  }
 
-    if (!doctor) {
-        throw new ApiError(400, "doctor does not found")
-    }
+  req.doctor = doctor;
 
-    req.doctor = doctor
-
-    next()
-})
+  next();
+});
--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>page</div>
-  )
+"use client";
+import Loading from "@/components/Loading";
+import api from "@/utils/api";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+interface Patient {
+  _id: string;
+  patientName: string;
+  age: number;
+  gender: "MALE" | "FEMALE" | "OTHER";
+  diagnosis: string;
+  medicine: string;
+  address?: string;
+  diet?: "VEG" | "NON VEG" | "MIXED";
+  phoneNumber: string;
+  familySize?: number;
+  occupation?: string;
+  followUpDate?: Date;
 }
+const Page = () => {
+  const [patient, setPatient] = useState<Patient[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const router = useRouter();
 
-export default page
+  const functionFetchData = async () => {
+    const patientData = await api.get("/patient/all-patient");
+    setPatient(patientData.data.data);
+  };
+
+  useEffect(() => {
+    try {
+      if (!localStorage.getItem("doctorJWT")) router.push("login");
+      const load = async () => {
+        setLoading(true);
+        await functionFetchData();
+        setLoading(false);
+      };
+      load();
+    } catch (error:any) {
+      setError(
+        error.response?.data?.message ||
+          "there is problem while fetching patients",
+      );
+    }
+  }, []);
+
+  return loading ? <Loading /> : <div>hello</div>;
+};
+
+export default Page;
